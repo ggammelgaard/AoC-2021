725dd58bd4afec9c704f97e35bc54338b6308597
nc-guag <guag@netcompany.com>
2021-12-07 15:25:55 +0100

    6th part 2

--- 6th/part2.py
+++ 6th/part2.py
@@ -7,8 +7,8 @@ def min_max(in_list):
 
 if __name__ == "__main__":
     # read input
-    f = open(r"example.txt")
-    # f = open(r"input.txt")
+    # f = open(r"example.txt")
+    f = open(r"input.txt")
     lines = f.readlines()
     clean_lines = [s.rstrip('\n') for s in lines]
     # print(clean_lines)
@@ -16,17 +16,18 @@ if __name__ == "__main__":
     fish_ages = list(map(int, clean_lines[0].split(',')))
     fish_ages = np.array(fish_ages)
 
-    days = 80
-    for day in range(1, days+1):
-        fish_ages -= 1
-        birth_fishes = np.where(fish_ages == -1)[0]
-        if birth_fishes.size != 0:
-            fish_ages[birth_fishes] = 6
-            fish_ages = np.append(fish_ages, [8]*birth_fishes.size)
-        print("After {:2} days: {}".format(day, fish_ages))
+    days = 256
 
-    print("After {:2} days: Total of {} fish".format(days, fish_ages.size))
+    # fill intial array
+    fish_sorted = np.array([0] * 9).astype(np.longlong)
+    for fish in fish_ages:
+        fish_sorted[fish] += 1
 
+    # simulate
+    for day in range(1, days + 1):
+        birth_fishes = fish_sorted[0]
+        fish_sorted = np.roll(fish_sorted, -1)
+        fish_sorted[-1] = birth_fishes
+        fish_sorted[6] = fish_sorted[6] + birth_fishes
 
-    # TODO: Lav det om så man har en counter for hver alder en fisk kan have
-    # Så skal jeg ikke rode med arrays
+    print("After {:2} days: Total of {} fish".format(days, np.sum(fish_sorted)))
