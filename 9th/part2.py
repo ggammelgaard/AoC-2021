7a8138096c162275694bf622b000d6612d367ff6
GustavST <gulle_g@hotmail.com>
2021-12-09 22:09:08 +0100

    Small changes

--- 9th/part2.py
+++ 9th/part2.py
@@ -15,8 +15,8 @@ def low_point(hmap, x, y):
 
 if __name__ == "__main__":
     # read input
-    f = open(r"example.txt")
-    # f = open(r"input.txt")
+    # f = open(r"example.txt")
+    f = open(r"input.txt")
     lines = f.readlines()
     clean_lines = [s.rstrip('\n') for s in lines]
     # print(clean_lines)
