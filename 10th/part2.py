710307f8fcbe18b374599a383406ab030919ff96
nc-guag <guag@netcompany.com>
2021-12-10 15:39:17 +0100

    10th WIP

---
+++ 10th/part2.py
@@ -0,0 +1,8 @@
+
+if __name__ == "__main__":
+    # read input
+    # f = open(r"example.txt")
+    f = open(r"input.txt")
+    lines = f.readlines()
+    clean_lines = [s.rstrip('\n') for s in lines]
+    # print(clean_lines)
