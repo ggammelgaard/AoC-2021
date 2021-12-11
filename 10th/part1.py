9388d3085fc4ec00c5c1fce86d92254e2cea30e5
GustavST <gulle_g@hotmail.com>
2021-12-11 10:04:49 +0100

    10th of december

--- 10th/part1.py
+++ 10th/part1.py
@@ -1,106 +1,34 @@
-CHAR_MAP = ['()', '[]', '{}', '<>']
-ipar = 0
-ifir = 1
-itub = 2
-ikro = 3
+CHAR_DICT = {')': '(', ']': '[', '}': '{', '>': '<', }
+SCORE_DICT = {')': 3, ']': 57, '}': 1197, '>': 25137, }
 
 
-def check_inconsistensies(counter, querying_index, char, line, i):
-    for j in range(4):
-        if j == querying_index:
-            continue
-
-        if counter[j] != 0:
-            print("ERROR! Char '{}' interrupts".format(char, CHAR_MAP[j], i))
-            print(line[:i + 1])
-            return False
-    return True
-
-
-def find_scope(_line, end_idx, querying_index):
-    start_idx = 0
-    counter = 0
-    for j in range(end_idx, 0, -1):
-        if _line[j] == CHAR_MAP[querying_index][0]:
-            counter += 1
-        elif _line[j] == CHAR_MAP[querying_index][1]:
-            counter -= 1
-
-        if counter == 0:
-            start_idx = j
-            break
-
-    return _line[start_idx:end_idx + 1]
-
-
-def check_line(line):
-    counter = [0, 0, 0, 0]
-
-    for i in range(len(line)):
-        char = line[i]
-
-        if char == CHAR_MAP[ipar][0]:
-            counter[ipar] += 1
-        elif char == CHAR_MAP[ipar][1]:
-            counter[ipar] -= 1
-            if not check_inconsistensies(counter, ipar, char, line, i):
-                return False
-
-        elif char == CHAR_MAP[ifir][0]:
-            counter[ifir] += 1
-        elif char == CHAR_MAP[ifir][1]:
-            counter[ifir] -= 1
-            if not check_inconsistensies(counter, ifir, char, line, i):
-                return False
-
-        elif char == CHAR_MAP[itub][0]:
-            counter[itub] += 1
-        elif char == CHAR_MAP[itub][1]:
-            counter[itub] -= 1
-            if not check_inconsistensies(counter, itub, char, line, i):
-                return False
-
-        elif char == CHAR_MAP[ikro][0]:
-            counter[ikro] += 1
-        elif char == CHAR_MAP[ikro][1]:
-            counter[ikro] -= 1
-            if not check_inconsistensies(counter, ikro, char, line, i):
-                return False
-    return True
+def error_msg(_char, idx):
+    print("ERROR! Char '{}' interrupts at i={}".format(char, idx))
 
 
 if __name__ == "__main__":
     # read input
-    f = open(r"example.txt")
-    # f = open(r"input.txt")
+    # f = open(r"example.txt")
+    f = open(r"input.txt")
     lines = f.readlines()
     clean_lines = [s.rstrip('\n') for s in lines]
-    print(clean_lines)
+    # print(clean_lines)
 
+    score = 0
+    scope_list = []
     for line in clean_lines:
         for i in range(len(line)):
             char = line[i]
 
-            if char == CHAR_MAP[ipar][1]:
-                scope = find_scope(line, i, ipar)
-                print(scope)
-                if not check_line(scope):
+            if char in ['(', '[', '{', '<']:
+                scope_list.append(char)
+
+            elif char in [')', ']', '}', '>', ]:
+                if scope_list[-1] == CHAR_DICT[char]:
+                    scope_list.pop()
+                else:
+                    # error_msg(char, i)
+                    score += SCORE_DICT[char]
                     break
 
-            if char == CHAR_MAP[ifir][1]:
-                scope = find_scope(line, i, ifir)
-                print(scope)
-                if not check_line(scope):
-                    break
-
-            if char == CHAR_MAP[itub][1]:
-                scope = find_scope(line, i, itub)
-                print(scope)
-                if not check_line(scope):
-                    break
-
-            if char == CHAR_MAP[ikro][1]:
-                scope = find_scope(line, i, ikro)
-                print(scope)
-                if not check_line(scope):
-                    break
+    print("Final score: {}".format(score))
--- 10th/part2.py
+++ 10th/part2.py
@@ -1,3 +1,12 @@
+import numpy as np
+
+CHAR_DICT = {')': '(', ']': '[', '}': '{', '>': '<', }
+SCORE_DICT_AC = {'(': 1, '[': 2, '{': 3, '<': 4}
+
+
+def error_msg(_char, idx):
+    print("ERROR! Char '{}' interrupts at i={}".format(char, idx))
+
 
 if __name__ == "__main__":
     # read input
@@ -6,3 +15,30 @@ if __name__ == "__main__":
     lines = f.readlines()
     clean_lines = [s.rstrip('\n') for s in lines]
     # print(clean_lines)
+
+    score_list = []
+    for line in clean_lines:
+        score = 0
+        scope_list = []
+        for i in range(len(line)):
+            char = line[i]
+
+            if char in ['(', '[', '{', '<']:
+                scope_list.append(char)
+
+            elif char in [')', ']', '}', '>', ]:
+                if scope_list[-1] == CHAR_DICT[char]:
+                    scope_list.pop()
+                else:
+                    # error_msg(char, i)
+                    # score += SCORE_DICT[char]
+                    break
+
+            if i == len(line) - 1:
+                for cchar in reversed(scope_list):
+                    # print(cchar)
+                    score = score * 5 + SCORE_DICT_AC[cchar]
+                score_list.append(score)
+
+    # print(score_list)
+    print(int(np.median(score_list)))
